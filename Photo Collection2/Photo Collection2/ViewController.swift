ad83eed52aa4aa7a2b4aad123a8f18ea68afd8a8
Jordan Davis <jordan.m.davis92@gmail.com>
2019-05-15 18:09:16 -0400

    initial commit
    added xcode project and README.md file to repository

---
+++ Photo Collection2/Photo Collection2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Collection2
+//
+//  Created by Jordan Davis on 5/15/19.
+//  Copyright © 2019 Jordan Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
