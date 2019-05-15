d5e04d24b577ae8ad2fda14d91262c4640c212bc
Jordan Davis <jordan.m.davis92@gmail.com>
2019-05-15 18:24:49 -0400

    Model & Controller
    set up the struct and class for the model and model controller

---
+++ Photo Collection2/Photo Collection2/Model & Model Controller/Photo.swift
@@ -0,0 +1,20 @@
+//
+//  Photo.swift
+//  Photo Collection2
+//
+//  Created by Jordan Davis on 5/15/19.
+//  Copyright © 2019 Jordan Davis. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    
+    var imageData: Data
+    var title: String
+    
+    init(imageData: Data, title: String) {
+        self.imageData = imageData
+        self.title = title
+    }
+}
