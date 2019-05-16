35aff1d69ad824f93b3c966cea075f5a8ce6e059
Jordan Davis <jordan.m.davis92@gmail.com>
2019-05-15 22:37:35 -0400

    Stretch Goal
    added persistence

--- Photo Collection2/Photo Collection2/Model & Model Controller/Photo.swift
+++ Photo Collection2/Photo Collection2/Model & Model Controller/Photo.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Photo: Equatable {
+struct Photo: Codable, Equatable {
     
     var imageData: Data
     var title: String
--- Photo Collection2/Photo Collection2/Model & Model Controller/PhotoController.swift
+++ Photo Collection2/Photo Collection2/Model & Model Controller/PhotoController.swift
@@ -13,15 +13,55 @@ class PhotoController {
     func createPhoto(imageData: Data, title: String) {
         let photo = Photo(imageData: imageData, title: title)
         photos.append(photo)
+        
+        saveToPersistentStore()
     }
     
     func updatePhoto(photo: Photo, imageData: Data, title: String) {
         guard let index = photos.firstIndex(of: photo) else { return }
         photos[index].imageData = imageData
         photos[index].title = title
+        
+        saveToPersistentStore()
+    }
+    
+    //MARK: -Persistence
+    
+    func saveToPersistentStore() {
+        
+        guard let url = photoCollectionURL else { return }
+        do {
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(photos)
+            try data.write(to: url)
+        } catch {
+            print("Error saving photos: \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        
+        let fileManger = FileManager.default
+        guard let url = photoCollectionURL, fileManger.fileExists(atPath: url.path) else { return }
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            let decodedPhotos = try decoder.decode([Photo].self, from: data)
+            photos = decodedPhotos
+        } catch {
+            print("Error loading from disk: \(error)")
+        }
     }
     
     //MARK: Properties:
     
     var photos: [Photo] = []
+    
+    private var photoCollectionURL: URL? {
+        let fileManager = FileManager.default
+        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        print("Documents: \(documents.path)")
+        return documents.appendingPathComponent("PhotoCollection.plist")
+    }
 }
